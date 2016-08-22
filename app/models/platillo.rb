e7b4650b729d5b0d2b053641709524d45f893758
luisesh11 <luisesh1x10@hotmail.com>
2016-08-22 07:36:00 +0000

    solucio n heroku

--- app/models/platillo.rb
+++ app/models/platillo.rb
@@ -1,11 +1,5 @@
 class Platillo < ActiveRecord::Base
-  has_attached_file :cover , styles:{:mediano=>"1280x720",:tumb=>"800x600"}
-  validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
-  has_attached_file :map, :styles => { :medium => "238x238>", 
-                                   :thumb => "100x100>"
-                                 }
   
-  validates_attachment_content_type :map, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, :message => 'file type is not allowed (only jpeg/png/gif images)'                              
   belongs_to :category
   belongs_to :group
   has_many :saucerOrders

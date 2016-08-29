ec5a8926edab4a61bba9b0f025ad36946d4d58d7
luisesh11 <luisesh1x10@hotmail.com>
2016-08-29 01:36:01 +0000

    primera actualisacion de sanjon en server

--- db/migrate/20160821212108_add_cover_to_platillos.rb
+++ db/migrate/20160821212108_add_cover_to_platillos.rb
@@ -1,6 +1,7 @@
 class AddCoverToPlatillos < ActiveRecord::Migration
   def up
     add_attachment :platillos, :cover
+    
   end
 
   def down
--- db/migrate/20160826154836_add_anotaciones_to_customer.rb
+++ db/migrate/20160826154836_add_anotaciones_to_customer.rb
@@ -1,5 +1,6 @@
 class AddAnotacionesToCustomer < ActiveRecord::Migration
   def change
     add_column :customers, :anotaciones, :text
+    
   end
 end

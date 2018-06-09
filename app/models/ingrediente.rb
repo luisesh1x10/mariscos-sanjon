db5f8c0e9f41b402020cde1519b1af68dc8e66c8
luisesh11 <luisesh1x10@hotmail.com>
2018-06-09 04:05:27 -0600

    agregar ingredientes a sucurasales

--- app/models/ingrediente.rb
+++ app/models/ingrediente.rb
@@ -3,4 +3,11 @@ class Ingrediente < ActiveRecord::Base
   has_many :inventarios
   has_many :ingredients
   has_many :expenses
+  after_create :agregar_a_inventarios
+  
+  def agregar_a_inventarios
+    Sucursal.all.each do |s|
+      Inventario.create(ingrediente_id:self.id,sucursal_id:s.id)
+    end
+  end
 end
